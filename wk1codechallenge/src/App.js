ef07c9fcff1eaeaa867b8f06716a5c4a93abcc8f
Bridget <bridget.njoki@student.moringaschool.com>
2024-08-06 22:36:35 +0300

    First commit

---
+++ wk1codechallenge/src/App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import TransactionTable from './Components/TransactionTable';
+import TransactionForm from './Components/TransactionForm';
+
+
+function App() {
+  
+}
+
+export default App;
---
+++ wk1codechallenge/src/Components/TransactionForm.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+
+function TransactionForm({ addTransaction }) {
+    const [formData, setFormData]= useState({
+        date: '',
+        description: '',
+        category: '',
+        amount: '',
+
+    });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch('http://localhost:3000/transactions', {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        })
+        .then((response) => response.json())
+        .then((newTransaction) =>{
+            addTransaction(newTransaction);
+            setFormData({ date: '', description: '', category:'', amount: ''});
+        });
+    };
+  return (
+    <form onSubmit={handleSubmit}>
+        
+    </form>
+  );
+}
+
+export default TransactionForm;
